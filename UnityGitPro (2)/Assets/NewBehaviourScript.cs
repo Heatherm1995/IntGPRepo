49af8c347017c8aed9269705a3b71cc596d134c3
Heatherm1995 <heather_macdonald@hotmail.co.uk>
2015-10-02 09:50:26 +0100

    test branch
    script

--- UnityGitPro (2)/Assets/NewBehaviourScript.cs
+++ UnityGitPro (2)/Assets/NewBehaviourScript.cs
@@ -6,7 +6,8 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
-	}
+	}	print("Hello World");
+
 	
 	// Update is called once per frame
 	void Update () {
